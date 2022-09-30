9e2098a2eb4600b469faead40676257c22b83682
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2022-09-30 16:00:55 +0100

    dummy web server

--- planets/index.js
+++ planets/index.js
@@ -1,13 +1,14 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanet = [];
 
 const isHabitablePlanet = (planet) => {
   return (
     planet["koi_disposition"] === "CONFIRMED" &&
     planet["koi_insol"] > 0.36 &&
-    planet["koi_insol"] < 1.11
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
   );
 };
 
@@ -20,13 +21,16 @@ fs.createReadStream("kepler_data.csv")
   )
   .on("data", (data) => {
     if (isHabitablePlanet(data)) {
-      results.push(data);
+      habitablePlanet.push(data);
     }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(results);
-    console.log("done");
+    console.log(
+      habitablePlanet.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
   });
