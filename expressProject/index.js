f90cecbc36bd1911fc473891fca913fd4217c1f5
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2022-10-13 17:15:52 +0100

    organizing  the code in MVC

---
+++ expressProject/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const friendsRouter = require("./routes/friends.router");
+const messagesRouter = require("./routes/messages.router");
+
+const app = express();
+const PORT = 3000;
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  // actions go here...
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.baseUrl} ${req.url} ${delta}`);
+});
+
+app.use(express.json());
+app.use("/friends", friendsRouter);
+app.use("/messages", messagesRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
---
+++ expressProject/models/friends.model.js
@@ -0,0 +1,12 @@
+const friends = [
+  {
+    id: 0,
+    name: "Albert",
+  },
+  {
+    id: 1,
+    name: "John ",
+  },
+];
+
+module.exports = friends;
