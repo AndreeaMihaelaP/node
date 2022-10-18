2adbfdafee4d2f3837ddf8fd36be7a4203eaf0cf
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2022-10-18 17:27:33 +0100

    adding express template hbs and image in node

--- expressProject/controllers/messages.controller.js
+++ expressProject/controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const path = require("path"); // \folders\files.js
+
 function getMessages(req, res) {
-  res.send("<ul><li>Hello Albert!</li></ul>");
+  res.sendFile(
+    path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+  );
 }
 
 function postMessage(req, res) {
--- expressProject/index.js
+++ expressProject/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const friendsRouter = require("./routes/friends.router");
 const messagesRouter = require("./routes/messages.router");
 
 const app = express();
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -14,7 +18,15 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl} ${req.url} ${delta}`);
 });
 
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.render("index", {
+    title: "ma friends",
+    caption: "France",
+  });
+});
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
 
